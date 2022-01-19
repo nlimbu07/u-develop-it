a32ceef7ff09bb4df4e9dab43695657daf61883e
Niraj Limbu <nlimbu@yahoo.com>
2022-01-19 17:43:49 -0500

    save queries in schema and seed files

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,14 +5,14 @@ VALUES
   ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
 
 INSERT INTO candidates (first_name, last_name, industry_connected)
-VALUES 
-('Ronald', 'Firbank', 1),
-('Virginia', 'Woolf', 1),
-('Piers', 'Gaveston', 0),
-('Charles', 'LeRoi', 1),
-('Katherine', 'Mansfield', 1),
-('Dora', 'Carrington', 0),
-('Edward', 'Bellamy', 0),
-('Montague', 'Summers', 1),
-('Octavia', 'Butler', 1),
-('Unica', 'Zurn', 1);
+VALUES
+  ('Ronald', 'Firbank', 1),
+  ('Virginia', 'Woolf', 1),
+  ('Piers', 'Gaveston', 0),
+  ('Charles', 'LeRoi', 1),
+  ('Katherine', 'Mansfield', 1),
+  ('Dora', 'Carrington', 0),
+  ('Edward', 'Bellamy', 0),
+  ('Montague', 'Summers', 1),
+  ('Octavia', 'Butler', 1),
+  ('Unica', 'Zurn', 1);
